7186458642920dd4a3756f938f78e387919a9068
Michel Iwaniec <michel_iwaniec@yahoo.com>
2025-01-09 01:15:34 +0000

    Add latest version of dependencies
    - FamiStudio Windows/Linux/Mac official 4.3.0 binaries
    
    - gbvm: https://github.com/michel-iwaniec/gbvm/tree/wip_nes_port_gbs4_rebase_retry-v4 (2271a14f063b927e196031b80d4af5c0396f8486)
    
    - gbdk win/linux/mac: https://github.com/michel-iwaniec/gbdk-2020/tree/nes-use-wram-for-DATA-segment-430-lcd_mapper_writes-8x8-v4 (a2c1f9ebebc4e84742440f37c548e766d32f873f)

--- buildTools/win32-x64/gbdk/examples/gb/gbprinter/compile.bat
+++ buildTools/win32-x64/gbdk/examples/gb/gbprinter/compile.bat
@@ -1,6 +1,6 @@
 REM Automatically generated from Makefile
 
-..\..\..\bin\png2asset res\scene00001.png -keep_duplicate_tiles -map -bpp2 -c obj\res\scene00001.c
+..\..\..\bin\png2asset res\scene00001.png -keep_duplicate_tiles -map -bpp 2 -c obj\res\scene00001.c
 ..\..\..\bin\lcc -Iobj  -c -o obj\scene00001.o obj\res\scene00001.c
 ..\..\..\bin\lcc -Iobj -c -o obj\gbprinter.o src\gbprinter.c
 ..\..\..\bin\lcc -Iobj -c -o obj\print_example.o src\print_example.c
